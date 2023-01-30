fc6c6b74562c6d20f6d449afcea2f1a7d6a4945a
유동환 <ydhwan0905@gmail.com>
2023-01-30 11:32:28 +0900

    Create Q14681.java

---
+++ BackJoon/src/com/dong/flow/Q14681.java
@@ -0,0 +1,25 @@
+package com.dong.flow;
+
+import java.util.Scanner;
+
+public class Q14681 {
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		int x = scan.nextInt();
+		int y = scan.nextInt();
+		
+				
+		if(x > 0) {
+			if(y > 0)System.out.println(1);
+			else System.out.println(4);
+		} 
+		if(x < 0) {
+			if(y > 0)System.out.println(2);
+			else System.out.println(3);
+		} 
+		scan.close();
+				
+	}
+}
