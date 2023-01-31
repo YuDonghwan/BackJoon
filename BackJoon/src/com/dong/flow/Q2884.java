6cfca29db78d21a90649b1c1243d9c1ac0680f0d
유동환 <ydhwan0905@gmail.com>
2023-01-31 09:58:05 +0900

    Create Q2884.java

---
+++ BackJoon/src/com/dong/flow/Q2884.java
@@ -0,0 +1,32 @@
+package com.dong.flow;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Q2884 {
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		int h = scan.nextInt();
+		
+		int m = scan.nextInt();
+		
+		
+		if(m<45) {
+			h = h-1;
+			if(h<0) h =23;
+			m = 60 - (45-m);
+			
+		}else {
+			m = m-45;
+		}
+		System.out.print(h + " " + m);
+		scan.close();
+		
+		
+		
+		
+	}
+}
