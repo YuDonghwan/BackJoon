a1bdad631138972b2836f76e5bb41c8bd6c8dbbe
유동환 <ydhwan0905@gmail.com>
2023-02-02 22:50:36 +0900

    Update Q2525.java

--- BackJoon/src/com/dong/flow/Q2525.java
+++ BackJoon/src/com/dong/flow/Q2525.java
@@ -3,24 +3,23 @@ package com.dong.flow;
 import java.util.Scanner;
 
 public class Q2525 {
-	
+
 	public static void main(String[] args) {
-		
-		
+
 		Scanner scan = new Scanner(System.in);
-		
+
 		int h = scan.nextInt();
 		int m = scan.nextInt();
 		int c = scan.nextInt();
-		
+
 		m += c;
-		if(m / 60 > 0) {
-			h += m/60;
+		if (m / 60 > 0) {
+			h += m / 60;
 			m %= 60;
 			h %= 24;
 		}
 		System.out.println(h + " " + m);
-		
+
 		scan.close();
 	}
 }
