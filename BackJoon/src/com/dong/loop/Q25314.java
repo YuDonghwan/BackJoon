13a1e92eeb3ecc3d3ee99a82b5055f8d1940e091
유동환 <ydhwan0905@gmail.com>
2023-02-21 23:59:16 +0900

    Create Q25314.java

---
+++ BackJoon/src/com/dong/loop/Q25314.java
@@ -0,0 +1,18 @@
+package com.dong.loop;
+
+import java.util.Scanner;
+
+public class Q25314 {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner scan = new Scanner(System.in);
+		long a = scan.nextInt();
+		long b = scan.nextInt();
+		long c = scan.nextInt();
+        
+		System.out.println(a + b + c);
+		scan.close();
+	}
+}
