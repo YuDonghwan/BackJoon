664dc2b02c3ea48861d3c4fdc5615e5bfb23ee3e
dong <ydhwan0905@gmail.com>
2023-02-28 09:28:41 +0900

    Create Q10807.java

---
+++ BackJoon/src/com/dong/array/Q10807.java
@@ -0,0 +1,50 @@
+package com.dong.array;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+
+public class Q10807 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		String[] arr = new String[n];
+		int[] arr2 = new int[arr.length];
+		int count = 0;
+		arr = br.readLine().split(" ");
+		
+		int tmp = Integer.parseInt(br.readLine());
+		for(int i=0; i< arr.length; i++) {
+			arr2[i] = Integer.parseInt(arr[i]);
+		}
+		
+		for(int i=0; i<arr2.length; i++) {
+			if(arr2[i] == tmp) count++;
+		}
+		
+		bw.write(String.valueOf(count));
+		
+		
+		
+		
+		
+		
+		
+		
+		br.close();
+		bw.flush();
+		bw.close();
+		
+		
+		
+	}
+	
+}
